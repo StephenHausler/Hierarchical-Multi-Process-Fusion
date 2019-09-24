cdce1390d01a6fdb238bfff9ef7637ab482b4e1a
StephenHausler <43105722+StephenHausler@users.noreply.github.com>
2019-09-24 10:41:03 +1000

    Add files via upload

---
+++ computeAllFeats.m
@@ -0,0 +1,35 @@
+clc
+clear
+close all
+
+addpath('Class_Files');
+addpath('Load_Files');
+addpath('Helper_Funcs');
+
+Dataset = 'Nordland';
+%Dataset = 'Berlin';
+
+saveFolder = 'Features_Nordland_Cont/';
+%saveFolder = 'Features_HPCTest_diffResTest/';
+
+mkdir(saveFolder);
+
+HPC = 1;
+Win = 0;
+
+%run all of this on HPC
+Time_CNNHeat = Load_CNNHeat(Dataset,saveFolder,HPC,Win);
+Time_CNNWhole = Load_CNNWhole(Dataset,saveFolder,HPC,Win);
+Time_Gist = Load_Gist(Dataset,saveFolder,HPC,Win);
+Time_HOG = Load_HOG(Dataset,saveFolder,HPC,Win);
+Time_KAZE = Load_KAZE(Dataset,saveFolder,HPC,Win);
+Time_NetVLAD = Load_NetVLAD(Dataset,saveFolder,HPC,Win);
+Time_SAD = Load_SAD(Dataset,saveFolder,HPC,Win);
+Time_SURF = Load_SURF(Dataset,saveFolder,HPC,Win);
+Time_OLO = Load_OLO(Dataset,saveFolder,HPC,Win);
+
+save('Load_Times_Nordland_TestSet.mat','Time_CNNWhole','Time_HOG','Time_KAZE',...
+    'Time_NetVLAD','Time_OLO','Time_SAD','Time_SURF','Time_Gist','Time_CNNHeat');
+
+
+
